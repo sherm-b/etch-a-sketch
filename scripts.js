6e00436ecefc3dd3f3ec42efad9d90e961dfff8a
jeremy-b <jeremy.steven.bess@gmail.com>
2022-08-02 22:37:27 -0700

    Draw single square div in grid parent div

---
+++ scripts.js
@@ -0,0 +1,8 @@
+
+let gridBody = document.getElementById('grid')
+const gridSquare = document.createElement('div')
+gridSquare.style.width = '40px';
+gridSquare.style.height = "40px";
+gridSquare.style.border = '1px solid black';
+
+gridBody.appendChild(gridSquare)
