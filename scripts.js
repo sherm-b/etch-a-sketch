814ccc8b9334af9ac403b83a6551e73f19fa4189
jeremy-b <jeremy.steven.bess@gmail.com>
2022-08-04 19:46:51 -0700

    Add radios as options for pen type

--- scripts.js
+++ scripts.js
@@ -1,24 +1,35 @@
-
-const gridBody = document.getElementById('grid')
-const resetButton = document.getElementById('reset-button')
-const sideInput = document.getElementById('side-length-input')
-const setButton = document.getElementById('side-length-set')
 let allSquares = document.getElementsByClassName('grid-square')
+const radioBlack = document.getElementById('black')
+const radioRainbow = document.getElementById('rainbow')
+
+const setButton = document.getElementById('side-length-set')
 setButton.addEventListener('click', makeGrid)
 
 function makeGrid(){
     clearSquares()
+    const gridBody = document.getElementById('grid')
+    const sideInput = document.getElementById('side-length-input')
     gridBody.style.gridTemplateColumns = `repeat(${sideInput.value}, 1fr)`;
     for(let i = 0;i < sideInput.value*sideInput.value;i++){
         let gridSquare = document.createElement('div');
         gridSquare.classList.add('grid-square');
-        gridSquare.addEventListener('mouseover', (e)  => {
+        if (radioBlack.checked){
+            gridSquare.addEventListener('mouseover', (e)  => {
             e.target.style.backgroundColor = 'black';
         })
-        gridBody.appendChild(gridSquare);
-        
+        } else if (radioRainbow.checked) {
+            let randomColor = Math.floor(Math.random()*16777215).toString(16);
+            gridSquare.addEventListener('mouseover', (e)  => {
+            e.target.style.backgroundColor = '#' + randomColor;
+        })
     }
+        gridBody.appendChild(gridSquare);
 }
+}
+
+radioBlack.addEventListener('click', makeGrid)
+radioRainbow.addEventListener('click', makeGrid)
+
 
 function clearSquares (){
     while (allSquares.length > 0){
@@ -29,7 +40,7 @@ function clearSquares (){
 }
 }
 
-
+const resetButton = document.getElementById('reset-button')
 function resetGrid(){
         for (i = 0; i < allSquares.length; i++){
             allSquares[i].style.backgroundColor = 'white'
